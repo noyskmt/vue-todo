b5ad6bbb500b68cf72ff722578484d469314f716
naoya sakamoto <n.sakamoto.sy@gmail.com>
2023-02-27 16:34:09 +0900

    inertia導入しました。

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,9 +10,11 @@ class UserController extends Controller
 {
   public function index()
   {
+
+    return Inertia::render('User/Index',['users' => User::all()]);
     // $adminUser = $this->userService->admin()->first();
-    $adminUser = app()->make('adminAccountService')->admin();
-    return response()->json($adminUser);
+    // $adminUser = app()->make('adminAccountService')->admin();
+    // return response()->json($adminUser);
 
     // return Inertia::render('Admin', [
     //   'adminUser' => $adminUser
---
+++ resources/js/Pages/User/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+defineProps({
+    users: Array,
+});
+</script>
+<template>
+    <header>
+        <ul>
+            <li><Link href="/dashboard">Dashboard</Link></li>
+        </ul>
+    </header>
+
+    <div class="p-4">
+        <h1 class="text-lg font-bold">ユーザ一覧</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+        </ul>
+    </div>
+</template>
